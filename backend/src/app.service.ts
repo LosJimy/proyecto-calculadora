dfa6f56a25e367a60633beb72069e8cf87e9e1b7
LosJimy <beo.erazoo@gmail.com>
2025-10-27 00:09:58 -0300

    operaciones

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -6,3 +6,41 @@ export class AppService {
     return 'Hello World!';
   }
 }
+
+export class Operaciones{
+  private numeros: number[];
+
+  constructor(numeros: number[]){
+    this.numeros = numeros;
+  }
+
+  sumarNumeros(): number{
+    let total = 0;
+    for(let numero of this.numeros){
+      total += numero;
+    }
+    return total;
+  }
+
+  restarNumeros(): number{
+    let total = this.numeros[0];
+    for(let i = 1; i<this.numeros.length-1; i++){
+      total -= this.numeros[i];
+    }
+    return total;
+  }
+
+  multiplicarNumeros(): number{
+    let total = this.numeros[0];
+    for(let i = this.numeros[1]; i<this.numeros.length-1; i++){
+      total = total * i;
+    }
+    return total;
+  }
+
+  dividirNumeros(): number{
+    let total = this.numeros[0]
+    
+    return total;
+  }
+}
